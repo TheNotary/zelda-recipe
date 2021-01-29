b0663fbe9b14d4fc3e30d114df0551e339bcec0d
TheNotary <no@email.plz>
2021-01-29 11:16:33 -0600

    basic setup

---
+++ java-api/src/main/java/org/njax/zeldarecipe/ZeldaRecipeApplication.java
@@ -0,0 +1,13 @@
+package org.njax.zeldarecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeldaRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeldaRecipeApplication.class, args);
+	}
+
+}
---
+++ java-api/src/test/java/org/njax/zeldarecipe/ZeldaRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.njax.zeldarecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeldaRecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
